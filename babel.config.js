f6aa30f3716fd37f4d6a64daf938194929cf2ec3
Mikasaak <1474691736@qq.com>
2023-09-24 22:47:56 +0800

    打包后不显示log

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
       libraryName: 'vant',
       libraryDirectory: 'es',
       style: (name) => `${name}/style/less`,
-    }, 'vant']
+    }, 'vant'],
+    'transform-remove-console'
   ]
 }
